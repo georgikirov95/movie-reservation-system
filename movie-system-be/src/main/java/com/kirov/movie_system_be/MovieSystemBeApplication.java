4507eb60b743918d4fc91a5dbf2cd1bda75efbfe
Georgi Kirov <georgipetrovkirov@gmail.com>
2025-08-20 14:45:16 +0300

    init backend

---
+++ movie-system-be/src/main/java/com/kirov/movie_system_be/MovieSystemBeApplication.java
@@ -0,0 +1,13 @@
+package com.kirov.movie_system_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieSystemBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieSystemBeApplication.class, args);
+	}
+
+}
---
+++ movie-system-be/src/test/java/com/kirov/movie_system_be/MovieSystemBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kirov.movie_system_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieSystemBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
