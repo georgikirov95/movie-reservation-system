0cc8532dd780105e774a9f8b217be1c01bbd1f24
Georgi Kirov <georgipetrovkirov@gmail.com>
2025-08-21 15:59:01 +0300

    feat: enable CORS on /movies/* routes

--- movie-system-be/src/main/java/com/kirov/movie_system_be/movies/MovieController.java
+++ movie-system-be/src/main/java/com/kirov/movie_system_be/movies/MovieController.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +16,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping(path = "/movies")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class MovieController {
   private final MovieService movieService;
 
