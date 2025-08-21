4c768a4a5ae0c9096c344952200252b4027dc105
Georgi Kirov <georgipetrovkirov@gmail.com>
2025-08-21 16:55:07 +0300

    feat: impl movie list in home

--- movie-system-fe/src/app/home/home.ts
+++ movie-system-fe/src/app/home/home.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, } from '@angular/core';
+import { Observable } from "rxjs";
+import { Loading } from "../shared/loading/loading";
+import { Movie } from "../models/movie";
+import { MovieService } from "../movie-service";
+import { AsyncPipe } from "@angular/common";
+import { MovieList } from "./movie-list/movie-list";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Loading, AsyncPipe, MovieList],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
+  movieCategories = ['Sci-Fi', 'Crime', 'Action', 'Drama']; // example categories
+  movies$: Observable<Movie[]>;
 
+  constructor(private movieService: MovieService) {
+    this.movies$ = this.movieService.getMovies();
+  }
 }
