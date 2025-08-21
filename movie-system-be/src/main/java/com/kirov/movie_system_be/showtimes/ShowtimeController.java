a7ce986be1bc7827ddead7c2f94d246766c7ff93
Georgi Kirov <georgipetrovkirov@gmail.com>
2025-08-21 18:09:46 +0300

    feat: enable CORS on /showtimes routes

--- movie-system-be/src/main/java/com/kirov/movie_system_be/showtimes/ShowtimeController.java
+++ movie-system-be/src/main/java/com/kirov/movie_system_be/showtimes/ShowtimeController.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping(path = "/movies/{movieId}/showtimes")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class ShowtimeController {
   private final ShowtimeService showtimeService;
 
